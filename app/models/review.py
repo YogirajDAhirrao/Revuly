a1e397db70d4ba3d74c94072bf1440b3dd28c442
YogirajDAhirrao <yogirajahirrao@gmail.com>
2026-01-13 14:47:34 +0530

    Addend embeddings in schema

--- app/models/review.py
+++ app/models/review.py
@@ -1,7 +1,19 @@
-from typing import Optional
+from typing import Optional, List
 from sqlmodel import SQLModel, Field
+from sqlalchemy import Column
+from sqlalchemy.dialects.postgresql import JSONB
 
-class Review(SQLModel,table=True):
-    id: Optional[int] = Field(default=None,primary_key=True)
+
+class Review(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
     text: str
     rating: Optional[int] = None
+
+    clean_text: Optional[str] = None
+    is_spam: bool = False
+
+    embedding: Optional[List[float]] = Field(
+        default=None,
+        sa_column=Column(JSONB)
+    )
