a84d240122ccdf386bdb7cbae53da5428d960ad3
YogirajDAhirrao <yogirajahirrao@gmail.com>
2026-01-13 17:40:28 +0530

    Feat: Added sentenceTransformer

--- app/services/review_service.py
+++ app/services/review_service.py
@@ -1,9 +1,21 @@
 from sqlmodel import Session, select
 from app.models.review import Review
 from app.schemas.review import ReviewCreate
+from app.ai.embeddings import get_embeddings
 
 def create_review(session: Session, review_data: ReviewCreate):
-    review = Review.model_validate(review_data)
+
+    clean_text = review_data.text.strip().lower()
+    embedding = get_embeddings(clean_text)
+
+    review = Review(
+        text = review_data.text,
+        clean_text = clean_text,
+        rating= review_data.rating,
+        is_spam=False,
+        embedding=embedding,
+    )
+
     session.add(review)
     session.commit()
     session.refresh(review)
